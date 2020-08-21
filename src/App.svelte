ebe59f2c857db4311560a85fdd13f7f412990802
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-21 16:54:21 +0100

    Boilerplate for making Svelte Electron apps

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import InternetPoints from "./InternetPoints.svelte";
+</script>
+
+<main>
+	<h1>Press button to get Internet Points</h1>
+	<InternetPoints />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/InternetPoints.svelte
@@ -0,0 +1,6 @@
+<script>
+  let points = 0;
+</script>
+
+<div>You have {points} Internet Points</div>
+<button on:click={() => points++}>Get Internet Point</button>
